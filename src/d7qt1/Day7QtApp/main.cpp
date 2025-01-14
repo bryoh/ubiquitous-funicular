5eb36abfab72a36fc16f0ee05b8372fbee0c62f1
brian.nyamu <brianbryo@gmail.com>
2025-01-14 09:58:24 +0000

    Revert "d6: pointers"
    This reverts commit e32a9eabe47ff4de67289f7e4147e93a1b00a2c2.

--- src/d7qt1/Day7QtApp/main.cpp
+++ src/d7qt1/Day7QtApp/main.cpp
@@ -1,11 +1,37 @@
-#include "mainwindow.h"
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QLineEdit>
 
-#include <QApplication>
+int main(int argc, char *argv[]) {
+  QApplication app(argc, argv);
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+  QMainWindow mainwindow;
+  mainwindow.setWindowTitle("Day 7: Layouts and Widgets");
+
+  QWidget *centralWidget = new QWidget();
+  QVBoxLayout *layout = new QVBoxLayout();
+
+  QLabel *label = new QLabel("Hello, Qt!");
+  QPushButton *button = new QPushButton("Click me!");
+  QLineEdit *editline = new QLineEdit();
+
+
+  layout ->addWidget(label);
+  layout ->addWidget(editline);
+  layout ->addWidget(button);
+
+  centralWidget ->setLayout(layout);
+  mainwindow.setCentralWidget(centralWidget);
+
+
+
+
+  mainwindow.resize(400, 300);
+  mainwindow.show();
+
+  return app.exec();
 }
